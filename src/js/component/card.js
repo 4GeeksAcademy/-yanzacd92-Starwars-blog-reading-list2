46205a734f0e7d0f428c284bccfbadb6744419c5
yanzacd92 <121258474+yanzacd92@users.noreply.github.com>
2023-04-27 02:51:25 +0000

    add images

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,22 +1,31 @@
 import propTypes from "prop-types";
 import React, {useContext} from "react";
 import {Context} from '../store/appContext'
-import rigoImage from "../../img/rigo-baby.jpg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 export const Card = (props) => {
-    const {actions} = useContext(Context)
+    const {store, actions} = useContext(Context)
+	const cards = store
 	return (
-		<div className="card">
-            <img src={props.img} className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">{props.name}</h5>
-                <div className="card-btns">
-                    <a href="#" className="learn-more btn btn-primary" onClick={() => actions.getDetails(props.url, props.element)}>Learn More!</a>
-                    <button className="add-favorite-btn btn btn-primary" type="submit" onClick={() => actions.addToFavorites((props.id + "-" + props.element), props.name, props.element)}><FontAwesomeIcon className="add-favorite" icon={faHeart} /></button>
-                </div>
-            </div>
+        <div className="list-group d-flex flex-column">
+            <h1 className="titles">{props.element}</h1>
+			<div key={props.element} className="card-list d-flex flex-row">
+				{cards[props.element]?.map((elm) =>
+					<div key={elm.uid}>
+						<div className="card">
+                            <img src={`https://starwars-visualguide.com/assets/img/${props.element}/${elm.uid}.jpg`} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">{elm.name}</h5>
+                                <div className="card-btns">
+                                    <a href="#" className="learn-more btn btn-primary" onClick={() => actions.getDetails(elm.url, props.element)}>Learn More!</a>
+                                    <button className="add-favorite-btn btn btn-primary" type="submit" onClick={() => actions.addToFavorites((elm.uid + "-" + props.element), elm.name, props.element)}><FontAwesomeIcon className="add-favorite" icon={faHeart} /></button>
+                                </div>
+                            </div>
+                        </div>
+					</div>
+				)}
+			</div>
         </div>
 	);
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -14,33 +14,10 @@ export const Home = () => {
 	}, [])
 	return (
 		<div className="text-center mt-5">
-			<div className="list-group d-flex flex-column">
-				<h1 className="titles">Planets</h1>
-				<div key="planets" className="card-list d-flex flex-row">
-					{cards.planets?.map((planet) =>
-						<div key={planet.uid}>
-							<Card id={planet.uid} name={planet.name} url={planet.url} element={"planets"} img={`https://starwars-visualguide.com/assets/img/planets/${planet.uid}.jpg`} />
-						</div>
-					)}
-				</div>
-				
-				<h1 className="titles">People</h1>
-				<div key="people" className="card-list d-flex flex-row">
-					{cards.people?.map((person) =>
-						<div key={person.uid}>
-							<Card id={person.uid} name={person.name} url={person.url} element={"people"} img={`https://starwars-visualguide.com/assets/img/characters/${person.uid}.jpg`} />
-						</div>
-					)}
-				</div>
-
-				<h1 className="titles">Vehicles</h1>
-				<div key="vehicles" className="card-list d-flex flex-row">
-					{cards.vehicles?.map((vehicle) =>
-						<div key={vehicle.uid}>
-							<Card id={vehicle.uid} name={vehicle.name} url={vehicle.url} element={"vehicles"} img={`https://starwars-visualguide.com/assets/img/vehicles/${vehicle.uid}.jpg`} />
-						</div>
-					)}
-				</div>
+			<div className="list-group d-flex flex-column">	
+				<Card element={"planets"} img={`https://starwars-visualguide.com/assets/img/planets/2.jpg`} />
+				<Card element={"people"} img={`https://starwars-visualguide.com/assets/img/characters/2.jpg`} />
+				<Card element={"vehicles"} img={`https://starwars-visualguide.com/assets/img/vehicles/4.jpg`} />
 			</div>
 		</div>
 )};
